edf2e6a89e70e3468bf4df1284324d7dd3f731e6
ArhanKonuksal <arhankonuksal@gmail.com>
2024-05-01 14:43:16 +0300

    kişiler (without css)

---
+++ src/components/Kişiler/Kişiler.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import Özten from "../../assets/Özten.jpg"
+import Mehmet from "../../assets/Mehmet.jpg"
+import "./Kişiler.css"
+
+const Kişiler = () => {
+  return (
+    <div className="kişiler">
+        <div className="right">
+            <div className="img-container">
+                <div className="img-stack top">
+                    <img src={Özten} className="img" alt="true"></img>
+                </div>
+            </div>
+            </div>
+            <div className="left">
+            <h1>Özten Özkoç</h1>
+            <p>gelecek<br/>yazılar</p>
+            <Link to="/İletişim">
+                <button className="btn">İletişim</button>
+            </Link>
+        </div>
+        
+        <div className="right"> 
+            <div className="img-container">
+                <div className="img-stack top">
+                    <img src={Mehmet} className="img" alt="true"></img>
+                </div>
+            </div>
+            </div>
+            <div className="left">
+            <h1>Mehmet Özkoç</h1>
+            <p>yazılar<br/>gelecek</p>
+            <Link to="/İletişim">
+                <button className="btn">İletişim</button>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Kişiler
--- src/pages/Hakkımızda.jsx
+++ src/pages/Hakkımızda.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import Footer from "../components/Footer/Footer"
+import Kişiler from '../components/Kişiler/Kişiler'
 
 const Hakkımızda = () => {
   return (
     <>
     <Navbar />
+    <Kişiler />
     <div className='heading'>
-      <h1>Hakkımızda</h1>
+      <h1>Fabrikamızı Keşfedin !</h1>
       <p>Merhaba! Biz Halışköy, lezzetli ve sağlıklı fırın ve süt ürünleri sunan bir şirketiz. Misyonumuz, müşterilerimize en kaliteli ürünleri sunmak ve onların damak zevklerine hitap etmektir. Yılların deneyimiyle, size en taze ve doğal ürünleri sunmak için çalışıyoruz. Bizimle tanışmaktan ve ürünlerimizi denemekten mutluluk duyacaksınız!</p>
     </div>
     <Footer />
